94b8a32d1e5f48dec325c732d04a431af81164d4
druscillah <walosang15@gmail.com>
2023-02-21 15:24:32 -0800

    nested loops 0-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number, +ve, -ve ,0
+ * @n: the number of the sign to be printed
+ * Return: 1 if the number is greater than zero
+ *0 if the number is 0
+ * -1 if the number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+{
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+iint putchar(char c);
+
+#endif
