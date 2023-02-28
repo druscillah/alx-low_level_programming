900bc9e6ddd782318c1951ef4fc11d3fc518083e
druscillah <walosang15@gmail.com>
2023-02-28 02:47:34 -0800

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,13 +7,10 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	int length;
 
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
+	for (length = 0; s[length] != '\0'; ++length)
+		_putchar(length);
+	
 	return (length);
 }
-
