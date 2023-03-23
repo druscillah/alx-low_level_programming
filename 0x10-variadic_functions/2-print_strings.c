0fdd3abc50b436c08c443024cc76fd4646032dd4
druscillah <walosang15@gmail.com>
2023-03-23 16:23:59 -0700

    task2 and 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,7 @@
  * print_strings - function that prints a string
  * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
- * Return: new line
+ * Return: nothing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
